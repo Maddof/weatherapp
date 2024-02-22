f46f3882e19c9957a3747a79cb76585e8bb622ce
Maddof <majd.azar88@gmail.com>
2024-02-22 16:38:03 +0100

    First UI draft design

--- src/index.js
+++ src/index.js
@@ -1,25 +1,48 @@
 import "./main.css";
 
-console.log("Hello world");
+const form = document.getElementById("search-form");
+const formInput = document.getElementById("search-form__city");
+const errorMsgPara = document.querySelector(".error-msg");
 
-const fetchUrl =
-  "http://api.weatherapi.com/v1/current.json?key=0b40ae3fcca84b2e9f6142531241902&q=London&aqi=no";
-
-async function getWeather(searchTag = "cat") {
+async function getWeather(city = "Stockholm") {
   try {
-    const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=0b40ae3fcca84b2e9f6142531241902&q=London&aqi=no`,
+    const responseForecast = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=0b40ae3fcca84b2e9f6142531241902&q=${city}&days=3&aqi=no&alerts=no
+      `,
       { mode: "cors", cache: "no-cache" }
     );
-    const weatherData = await response.json();
+    const weatherForecast = await responseForecast.json();
+
+    if (responseForecast.status === 400) {
+      console.log(responseForecast);
+      console.log(weatherForecast);
+      throw new Error(weatherForecast.error.message);
+    }
     // if (weatherData.data.length === 0) {
     //   errorDomMsg();
     //   throw new Error("Undefined (original)");
     // }
-    console.log(weatherData);
+    logForecast(weatherForecast);
+    errorMsgPara.textContent = "";
   } catch (error) {
+    displayErrorMsg(error);
     console.log("Error1: ", error);
   }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let searchCity = formInput.value;
+  getWeather(searchCity);
+  form.reset();
+});
+
+function logForecast(currentCityForecast) {
+  console.log(currentCityForecast);
+}
+
+function displayErrorMsg(errorMsg) {
+  errorMsgPara.textContent = errorMsg;
 }
 
 getWeather();
