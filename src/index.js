2ad2cad79d3c66e7d2fa7c18116840c486717a66
Maddof <majd.azar88@gmail.com>
2024-02-24 11:21:14 +0100

    changed fetch to https

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ async function getWeather(city = "Stockholm") {
   try {
     displayLoader();
     const responseForecast = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=0b40ae3fcca84b2e9f6142531241902&q=${city}&days=7&aqi=no&alerts=no
+      `https://api.weatherapi.com/v1/forecast.json?key=0b40ae3fcca84b2e9f6142531241902&q=${city}&days=7&aqi=no&alerts=no
       `,
       { mode: "cors", cache: "no-cache" }
     );
